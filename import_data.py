2378fa7eb4a535a801c4fe37f3ca541c3a522d9a
mgwright <mgwright@episd.org>
2017-01-19 15:14:45 -0700

    Added new user to default setup

--- import_data.py
+++ import_data.py
@@ -16,7 +16,9 @@ print 'deleted %s rows from User' % us
 session.commit()
 
 newuser = User(name='Marissa Mayer',email='yahoo@yahoo.com',username='MMayer',password='46ea0d5b246d2841744c26f72a86fc29')
+newuser2 = User(name='M Wright',email='mgwright@episd.org',username='mgwright',password='e99a18c428cb38d5f260853678922e03')
 session.add(newuser)
+session.add(newuser2)
 session.commit()
 
 newpost = Post(title='Edit this Post!',desc='Add your name to this post or delete it all together.',user_id=1)
